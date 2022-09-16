7c26db0fd279a6f71df7c84fdb1439f64d8ced34
Paulo Castro <paulovicg@outlook.com>
2022-09-16 15:29:22 -0400

    Adicionar setters/getters/construtor para Pedidos

--- Pedido.java
+++ Pedido.java
@@ -2,8 +2,36 @@ import java.util.ArrayList;
 
 public class Pedido {
     
-  TipoPagamento tipoPgt;
-  double totalPedido = 0;
-  ArrayList<Item> itens;
-    
+  private TipoPagamento tipoDePagamento;
+  private double totalPedido = 0;
+  private ArrayList<Item> itens;
+
+  public Pedido(TipoPagamento tipoPgt) {
+    itens = new ArrayList<Item>();
+    this.tipoDePagamento = tipoPgt;
+  }
+
+  public TipoPagamento getTipoPgt() {
+    return this.tipoDePagamento;
+  }
+
+  public void setTipoPgt(TipoPagamento tipoPgt) {
+    this.tipoDePagamento = tipoPgt;
+  }
+
+  public double getTotalPedido() {
+    return this.totalPedido;
+  }
+
+  public void setTotalPedido(double totalPedido) {
+    this.totalPedido = totalPedido;
+  }
+
+  public ArrayList<Item> getItens() {
+    return this.itens;
+  }
+
+  public void setItens(ArrayList<Item> itens) {
+    this.itens = itens;
+  }
 }
