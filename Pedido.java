0505ad4fc73126f9e80bbc9a3cbc483b8a6f410c
leomartins98 <leomartins983@gmail.com>
2022-09-16 15:16:13 -0400

    Criação classes TipoPagamento e Item.

--- Pedido.java
+++ Pedido.java
@@ -1,3 +1,9 @@
+import java.util.ArrayList;
+
 public class Pedido {
+    
+  TipoPagamento tipoPgt;
+  double totalPedido = 0;
+  ArrayList<Item> itens;
     
 }
---
+++ TipoPagamento.java
@@ -0,0 +1,5 @@
+public enum TipoPagamento {
+  CARTAO,
+  PIX,
+  DINHEIRO,
+}
