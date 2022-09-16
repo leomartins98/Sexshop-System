4003af7092760e26b0e2827ec826c5041f9097b5
leomartins98 <leomartins983@gmail.com>
2022-09-16 16:09:42 -0400

    Implementação da Classe Item

--- Item.java
+++ Item.java
@@ -1,3 +1,17 @@
 public class Item {
-    
+    Produto prod;
+    private int qtdItens;
+    public Produto getProd() {
+        return prod;
+    }
+    public void setProd(Produto prod) {
+        this.prod = prod;
+    }
+    public int getQtdItens() {
+        return qtdItens;
+    }
+    public void setQtdItens(int qtdItens) {
+        this.qtdItens = qtdItens;
+    }
 }
+
