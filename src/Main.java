bc1965c96eadbd599ef47a94643f2ccb8fbf8737
Paulo Castro <paulovicg@outlook.com>
2022-09-19 02:15:37 -0400

    Remove o print das credenciais

--- src/Main.java
+++ src/Main.java
@@ -9,11 +9,6 @@ public class Main {
     public static void main(String[] args) {
         // Credenciais:
         Credenciais credenciais = new Credenciais("res/credenciais.txt");
-        credenciais.update("Paulo", new Credencial("1234", "Paulo", "12345", true));
-        credenciais.salvarCredenciais();
-        // DEBUG:
-        for(Credencial c : credenciais.getCredenciais())
-            System.out.println(c.toString());
         
         // VIEWS:
         TelaLogin loginView = new TelaLogin();
