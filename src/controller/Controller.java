dfea7f7519b9d413b656d46ece09eb29073258ff
Paulo Castro <paulovicg@outlook.com>
2022-10-06 02:13:47 -0400

    Adiciona facade de popup

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,38 +1,19 @@
 package controller;
 
-import javax.swing.event.TableModelListener;
-import javax.swing.table.DefaultTableModel;
-
 import controller.listener_facade.LoginListenerFacade;
+import controller.listener_facade.PopupListenerFacade;
 import controller.listener_facade.RemoveListenerFacade;
 import controller.listener_facade.TableListenerFacade;
 
-import javax.swing.event.TableModelEvent;
-import javax.swing.JOptionPane;
-import javax.swing.JFrame;
-import java.awt.event.*;
-
 import serialization.CredentialManager;
 import serialization.ItemManager;
-import serialization.ProvedorManager;
-import serialization.SerializationManager;
-import loja.Produto;
 import credencial.*;
 import loja.Item;
 
-import view.TelaCadastroInvent;
-import view.TelaCadastroVenda;
-import view.TelaCadastroColab;
-import view.TelaCadastroFornecedor;
 import view.TelaAdmin;
 import view.TelaLogin;
 
 public class Controller {
-
-	private TelaCadastroVenda cadastroVenda;
-	private TelaCadastroInvent cadastroProduto;
-	private TelaCadastroColab cadastroView;
-	private TelaCadastroFornecedor cadastroFornecedor;
 
 	private TelaLogin loginView;
 	private TelaAdmin adminView;
@@ -44,6 +25,7 @@ public class Controller {
 	private RemoveListenerFacade removeListenerFacade;
 	private TableListenerFacade tableListenerFacade;
 	private LoginListenerFacade loginListenerFacade;
+	private PopupListenerFacade popupListenerFacade;
 
 	public Controller(TelaLogin login, TelaAdmin adminView, CredentialManager credenciais, ItemManager itemsLoja) {
 		this.loginView = login;
@@ -54,46 +36,21 @@ public class Controller {
 
 		this.initializeViews();
 		this.initializeModels();
-		this.initializeViewListeners();
 
 		// Facades:
 		removeListenerFacade = new RemoveListenerFacade(this.adminView, this.credenciais, this.itemsLoja);
 		tableListenerFacade = new TableListenerFacade(this.adminView, this.credenciais, this.itemsLoja);
 		loginListenerFacade = new LoginListenerFacade(this.loginView, this.adminView, this.credenciais);
+		popupListenerFacade = new PopupListenerFacade(this.adminView, this.loginView, this.credenciais, this.itemsLoja);
 
 		removeListenerFacade.execute();
 		tableListenerFacade.execute();
 		loginListenerFacade.execute();
+		popupListenerFacade.execute();
 	}
 
 	private void initializeViews() {
 		this.adminView.setLocationRelativeTo(null);
-
-		this.cadastroView = new TelaCadastroColab();
-		this.cadastroView.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-		this.cadastroView.setLocationRelativeTo(null);
-
-		this.cadastroFornecedor = new TelaCadastroFornecedor();
-		this.cadastroFornecedor.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-		this.cadastroFornecedor.setLocationRelativeTo(null);
-
-		this.cadastroProduto = new TelaCadastroInvent();
-		this.cadastroProduto.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-		this.cadastroProduto.setLocationRelativeTo(null);
-	}
-
-	private void initializeViewListeners() {
-		// Popup Listener:
-		this.adminView.addCredentialPopupListener(new CredentialPopupListener());
-		this.adminView.addProviderPopupListener(new ProviderPopupListener());
-		this.adminView.addProductPopupListener(new ProductPopupListener());
-
-		// Remove Listeners:
-		
-
-		// Cadastro View:
-		this.cadastroView.addCadastrarListener(new CadastrarListener());
-		this.cadastroProduto.addCadastrarProduto(new CadastrarProdutoListener());
 	}
 
 	// Initializers:
@@ -111,93 +68,4 @@ public class Controller {
 	public void execute() {
 		this.loginView.setVisible(true);
 	}
-
-	// **************** Listeners **************** \\
-
-	// Popup listeners:
-	class CredentialPopupListener implements ActionListener {
-
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			cadastroView.setVisible(true);
-		}
-
-	}
-
-	class ProductPopupListener implements ActionListener {
-
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			cadastroProduto.setVisible(true);
-		}
-	
-	}
-
-	class ProviderPopupListener implements ActionListener {
-
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			cadastroFornecedor.setVisible(true);
-		}
-
-	}
-
-	// Register listeners:
-	class CadastrarListener implements ActionListener {
-
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			var nome = cadastroView.getUser();
-			var usuario = cadastroView.getUsername();
-			var senha = cadastroView.getPassword();
-			var administrador = cadastroView.getAdm();
-
-			if(nome.isBlank() || usuario.isBlank() || senha.isBlank()) {
-				JOptionPane.showMessageDialog(loginView,
-					"Por favor, preencha todos os dados do funcionário.",
-					"Erro de Cadastro",
-					JOptionPane.ERROR_MESSAGE);
-				return;
-			}
-
-			DefaultTableModel model = (DefaultTableModel) adminView.getColabTable().getModel();
-			model.addRow(new Object[] { nome, usuario, senha, administrador ? "Administrador" : "Funcionário" });
-
-			credenciais.add(new Credencial(nome, usuario, senha, administrador));
-			credenciais.save();
-
-			cadastroView.clearView();
-			cadastroView.setVisible(false);
-		}
-	}
-
-	class CadastrarProdutoListener implements ActionListener {
-
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			int id = adminView.getProductTable().getRowCount();
-			String nome = cadastroProduto.getNome();
-			String preco = cadastroProduto.getPreco();
-			String descricao = cadastroProduto.getDescricao();
-			int qtd = cadastroProduto.getQuantidade();
-
-			if(nome.isBlank() || preco.isBlank() || descricao.isBlank()) {
-				JOptionPane.showMessageDialog(loginView,
-					"Por favor, preencha todos os dados do produto.",
-					"Erro de Cadastro",
-					JOptionPane.ERROR_MESSAGE);
-				return;
-			}
-
-			DefaultTableModel model = (DefaultTableModel) adminView.getProductTable().getModel();
-			model.addRow(new Object[] { id, nome, preco, descricao, qtd });
-
-			itemsLoja.add(new Item(new Produto(id, nome, Float.parseFloat(preco), descricao), qtd));
-			itemsLoja.save();
-
-			cadastroProduto.clearView();
-			cadastroProduto.setVisible(false);
-		}
-	}
-
 }
---
+++ src/controller/listener_facade/PopupListenerFacade.java
@@ -0,0 +1,150 @@
+package controller.listener_facade;
+
+import java.awt.event.*;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+
+import credencial.Credencial;
+import loja.Item;
+import loja.Produto;
+import serialization.CredentialManager;
+import serialization.ItemManager;
+import view.TelaAdmin;
+import view.TelaCadastroColab;
+import view.TelaCadastroFornecedor;
+import view.TelaCadastroInvent;
+import view.TelaLogin;
+
+public class PopupListenerFacade {
+
+    protected TelaCadastroColab workerRegisterView;
+    protected TelaCadastroInvent cadastroProduto;
+    protected TelaCadastroFornecedor cadastroFornecedor;
+
+    protected CredentialManager credenciais;
+    protected ItemManager itemsLoja;
+
+	protected TelaLogin loginView;
+    protected TelaAdmin adminView;
+
+    public PopupListenerFacade(TelaAdmin adminView, TelaLogin loginView, CredentialManager credenciais, ItemManager itemsLoja) {
+        this.adminView = adminView;
+        this.loginView = loginView;
+
+        this.credenciais = credenciais;
+        this.itemsLoja = itemsLoja;
+    }
+
+    public void execute() {
+        this.adminView.addCredentialPopupListener(new CredentialPopupListener());
+		this.adminView.addProviderPopupListener(new ProviderPopupListener());
+		this.adminView.addProductPopupListener(new ProductPopupListener());
+    }
+
+    public void initializeModels() {
+        // Colaboradores:
+        this.workerRegisterView = new TelaCadastroColab();
+		this.workerRegisterView.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+		this.workerRegisterView.setLocationRelativeTo(null);
+        this.workerRegisterView.addCadastrarListener(new CadastrarListener());
+
+        // Cadastro:
+        this.cadastroProduto = new TelaCadastroInvent();
+		this.cadastroProduto.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+		this.cadastroProduto.setLocationRelativeTo(null);
+        this.cadastroProduto.addCadastrarProduto(new CadastrarProdutoListener());
+
+        // Fornecedor:
+        this.cadastroFornecedor = new TelaCadastroFornecedor();
+		this.cadastroFornecedor.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+		this.cadastroFornecedor.setLocationRelativeTo(null);
+    }
+
+    // Popup Listeners:
+    class CredentialPopupListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			workerRegisterView.setVisible(true);
+		}
+
+	}
+
+	class ProductPopupListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			cadastroProduto.setVisible(true);
+		}
+	
+	}
+
+	class ProviderPopupListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			cadastroFornecedor.setVisible(true);
+		}
+
+	}
+
+    // Register Listeners:
+    class CadastrarListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			var nome = workerRegisterView.getUser();
+			var usuario = workerRegisterView.getUsername();
+			var senha = workerRegisterView.getPassword();
+			var administrador = workerRegisterView.getAdm();
+
+			if(nome.isBlank() || usuario.isBlank() || senha.isBlank()) {
+				JOptionPane.showMessageDialog(loginView,
+					"Por favor, preencha todos os dados do funcionário.",
+					"Erro de Cadastro",
+					JOptionPane.ERROR_MESSAGE);
+				return;
+			}
+
+			DefaultTableModel model = (DefaultTableModel) adminView.getColabTable().getModel();
+			model.addRow(new Object[] { nome, usuario, senha, administrador ? "Administrador" : "Funcionário" });
+
+			credenciais.add(new Credencial(nome, usuario, senha, administrador));
+			credenciais.save();
+
+			workerRegisterView.clearView();
+			workerRegisterView.setVisible(false);
+		}
+	}
+
+	class CadastrarProdutoListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			int id = adminView.getProductTable().getRowCount();
+			String nome = cadastroProduto.getNome();
+			String preco = cadastroProduto.getPreco();
+			String descricao = cadastroProduto.getDescricao();
+			int qtd = cadastroProduto.getQuantidade();
+
+			if(nome.isBlank() || preco.isBlank() || descricao.isBlank()) {
+				JOptionPane.showMessageDialog(loginView,
+					"Por favor, preencha todos os dados do produto.",
+					"Erro de Cadastro",
+					JOptionPane.ERROR_MESSAGE);
+				return;
+			}
+
+			DefaultTableModel model = (DefaultTableModel) adminView.getProductTable().getModel();
+			model.addRow(new Object[] { id, nome, preco, descricao, qtd });
+
+			itemsLoja.add(new Item(new Produto(id, nome, Float.parseFloat(preco), descricao), qtd));
+			itemsLoja.save();
+
+			cadastroProduto.clearView();
+			cadastroProduto.setVisible(false);
+		}
+	}
+}
